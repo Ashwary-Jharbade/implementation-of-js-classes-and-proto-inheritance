84bfb388593f9f4e7b449623b30520e7716eb1cc
Ashwary <ashwary.jharbade999@gmail.com>
2021-10-06 20:32:03 +0530

    Implemented classes inheritance and proto inheritance

--- assets/js/model.js
+++ assets/js/model.js
@@ -31,30 +31,43 @@ class Car extends Engine {
 
 const car = new Car("Bently", "$75000", "V8", "1200");
 console.log(car.getName(), car.getPrice(), car.getCC(), car.getEngine());
+// o/p - Bently $75000 1200 V8
 
 // Proto Inheritance
 let poke = {
-    name: "Garados",
-    attack: "Water Gun",
-    getName() {
-        return this.name;
-    },
-    getAttack() {
-        return this.getAttack;
+  pokemonNameList: ["Garados"],
+  pokemonAttackList: {
+    Garados: ["Water Gun", "Water ball"],
+  },
+  getPokemonList() {
+    return this.pokemonNameList;
+  },
+  getPokemonAttacks(name) {
+    if (this.pokemonAttackList.hasOwnProperty(name)) {
+      return this.pokemonAttackList[name];
     }
-}
+    return null;
+  },
+};
 
 let trainer = {
-    tname: "Satoshi",
-    age: 18,
-    getTName() {
-        return this.tname;
-    },
-    getAge() {
-        return this.age;
-    }
-}
+  name: "Satoshi",
+  age: 18,
+  getName() {
+    return this.name;
+  },
+  getAge() {
+    return this.age;
+  },
+};
 
-trainer.prototype = Object.create(poke);
+trainer.__proto__ = poke;
 
-console.log(trainer);
+console.log(trainer.name, trainer.age);
+// o/p - Satoshi 18
+
+console.log(trainer.pokemonNameList);
+// o.p - ['Garados']
+
+console.log(trainer.getPokemonAttacks(trainer.pokemonNameList[0]));
+// o/p -  ['Water Gun', 'Water ball']
